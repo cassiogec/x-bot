3b7f380e1787f1b8562b36259798f431bf23bdc9
Cássio Eckstein <cassiogec@gmail.com>
2024-12-13 10:28:49 -0300

    adding packages and creating structs for API call to GitHub API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use chrono::{DateTime, Local, Utc};
+use serde::Deserialize;
+use std::time::Instant;
+
+#[derive(Deserialize, Debug)]
+struct Commit {
+    url: String,
+    sha: String,
+    node_id: String,
+    html_url: String,
+    comments_url: String,
+    commit: CommitDetails,
+}
+
+#[derive(Deserialize, Debug)]
+struct CommitDetails {
+    url: String,
+    author: Contributor,
+    committer: Contributor,
+    message: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Contributor {
+    name: String,
+    email: String,
+    date: DateTime<Utc>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
